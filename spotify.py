dcb8e0235a34de0777148041f27f3ba91855115a
Kevin Messer <messer.kevin@gmail.com>
2018-05-18 11:15:13 +0200

    feat(utf-8): fix #1 : Accented titles are not shown
    Credits : AdamSpindle

--- spotify.py
+++ spotify.py
@@ -20,7 +20,7 @@ try:
 
     spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
     props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-    print(str(props['xesam:artist'][0]) + " - " + str(props['xesam:title']))
+    print(props['xesam:artist'][0] + " - " + props['xesam:title']).encode('utf-8')
     exit
 except dbus.exceptions.DBusException:
     exit
